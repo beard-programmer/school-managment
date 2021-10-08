54b6fe57a8867e8a92d338fd559f56b4e65973c7
Victor Shinkevich <supostatekx@gmail.com>
2021-10-08 14:46:31 +0300

    rename lessons repository in the plural

--- src/lesson/lessons.service.ts
+++ src/lesson/lessons.service.ts
@@ -9,28 +9,28 @@ import { LessonCreateInput } from './lesson-create.input';
 export class LessonsService {
   constructor(
     @InjectRepository(LessonEntity)
-    private lessonRepository: Repository<LessonEntity>,
+    private lessonsRepository: Repository<LessonEntity>,
   ) {}
 
   async getLesson(id: string): Promise<LessonEntity> {
-    return this.lessonRepository.findOne({ id: id });
+    return this.lessonsRepository.findOne({ id: id });
   }
 
   async getLessons(): Promise<LessonEntity[]> {
-    return this.lessonRepository.find();
+    return this.lessonsRepository.find();
   }
 
   async createLesson(
     lessonCreateInput: LessonCreateInput,
   ): Promise<LessonEntity> {
     const { name, startDate, endDate } = lessonCreateInput;
-    const lesson = this.lessonRepository.create({
+    const lesson = this.lessonsRepository.create({
       id: uuid(),
       name: name,
       startDate: startDate,
       endDate: endDate,
     });
 
-    return this.lessonRepository.save(lesson);
+    return this.lessonsRepository.save(lesson);
   }
 }
